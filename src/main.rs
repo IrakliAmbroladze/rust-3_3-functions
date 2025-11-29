63a8fcea59e2fda4f21a41264ead593fb21552fa
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-11-29 07:56:21 +0400

    feat: add several parameters ti the function

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
     println!("Hello, world!");
-    another_function(5);
+    another_function(5, 'h');
 }
 
-fn another_function(x: i32) {
-    println!("The value of x is: {x}");
+fn another_function(x: i32, unit_label: char) {
+    println!("The meadurement is: {x}{unit_label}");
 }
